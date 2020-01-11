b614a4afabffc4908010b9191b798fa4709a5a7e
Jack T <jackmt9@gmail.com>
2020-01-11 23:47:54 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(1, 9) && position_taken?(board, index)
+  if index.between?(1, 9) && !position_taken?(board, index)
     return true
   else return false
 end
